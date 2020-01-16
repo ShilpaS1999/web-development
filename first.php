70670137c5ba4006c791ea3e66b7ecdc84d467bb
ShilpaS1999 <shilpashreegr20@gmail.com>
2020-01-16 11:07:18 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="web.html" class="button">Home</a>
+    <style>
+        body{
+            background-image:url(https://i.ytimg.com/vi/cQWcMuJiDMQ/maxresdefault.jpg);
+            background-repeat:no-repeat;
+            background-position:top;
+        }
+        .button {
+  background-color: #008CBA;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+ margin-left:41%;
+ margin-top:35%;
+  cursor: pointer;
+  width:200px;
+}
+        </style>
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,49 @@
+
+<html>
+    <body>
+        <div class="abc">
+            <br>
+        <h1>LOGIN</h1>
+        <form action="db.php" method="post">
+            Name     :       <input type="text" name="username"><br><br>
+            Password:<input type="password" class="aa" name="password"><br><br><br>
+            <button class="button" input type="submit">Submit</button>
+</form>
+</div>
+<style>
+    body{
+        text-align:center;
+        font-size:large;
+        border-style:outset;
+        background-image:url(https://png.pngtree.com/thumb_back/fw800/back_pic/03/92/12/9057e226f57d435.jpg);
+       
+    }
+    input{
+        border-radius: 10px;
+        border-length:40%;
+        background-color:#e6e600;
+        font-size:large;
+    }
+    .abc{
+        background-color:#6666ff;
+        text-align:center;
+        height:350px;
+        width:30%;
+        border-style:solid;
+        font-size:x-large;
+  margin-top:180px;
+  margin-left:35%;
+  
+
+    }
+    .button{
+        font-size:20px;
+        background-color:#ffffff;
+        width:120px;
+    }
+    .aa{
+        width:200px;
+    }
+    </style>
+</body>
+</html>
